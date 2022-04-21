7c08ad22d4452bc83cad14cb694a18f4814d631a
guswnsakvk <guswnsakvk1@naver.com>
2022-04-21 11:51:36 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { Reset } from 'styled-reset'
+import logo from './image/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Reset />
+      <header className='header'>
+        <div className='headerTop'>
+          <ul className='myMenu'>
+            <li className='myMenu-margin'>Login</li>
+            <li className='myMenu-margin'>Join</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+        <div>
+          <img src={logo} alt="#" className='logo'></img>
+        </div>
+      </header>
+      <div>
+
+      </div>
     </div>
   );
 }
