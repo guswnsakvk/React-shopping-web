7269a542346753379d1d3ad928a0531d923a321c
guswnsakvk <guswnsakvk1@naver.com>
2022-04-21 12:33:49 +0900

    menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Reset } from 'styled-reset'
 import logo from './image/logo.png';
+// import blackfriday from './image/blackfriday.png'
 
 function App() {
   return (
@@ -18,8 +19,20 @@ function App() {
           <img src={logo} alt="#" className='logo'></img>
         </div>
       </header>
-      <div>
-
+      <div className='menus'>
+        <ul>
+          <li>Black Friday</li>
+          <li>Best</li>
+          <li>당일 배송</li>
+          <li>운동화</li>
+          <li>캔버스</li>
+          <li>워킹화</li>
+          <li>슬립온 슈즈</li>
+          <li>로퍼</li>
+          <li>부츠</li>
+          <li>구두</li>
+          <li>샌들</li>
+        </ul>
       </div>
     </div>
   );
