b15be230dad0d20a1816e5fe8a0a642f724b7b49
guswnsakvk <guswnsakvk1@naver.com>
2022-05-16 15:27:33 +0900

    cart버튼 여러번 누를 시 상품 개수가 계속 증가하는 버그 발생

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -17,7 +17,7 @@ function Detail(props){
     if(e.target.value !== ""){
       let check = false
       let copy = [...select]
-      let data = {product_size : e.target.value, product_cnt : 1}
+      let data = {product_size : e.target.value, product_cnt : 1, product_id: id}
       for(let i=0;i<select.length;i++){
         if(select[i].product_size === e.target.value){
           check = true
@@ -43,64 +43,74 @@ function Detail(props){
     }
   }
 
-  function setValue(params, e){
+  function setValue(index, e){
     let copy = [...select]
     let product_count = cnt
     let product_sum = sum
 
     if(findProduct.BlackFriday === 'O'){
-      product_sum -= copy[params].product_cnt * (findProduct.price / 2)
+      product_sum -= copy[index].product_cnt * (findProduct.price / 2)
       sumChange(product_sum)
     } else{
-      product_sum -= copy[params].product_cnt * findProduct.price
+      product_sum -= copy[index].product_cnt * findProduct.price
       sumChange(product_sum)
     }
-    product_count += e.target.value - copy[params].product_cnt
+    product_count += e.target.value - copy[index].product_cnt
     cntChange(product_count)
-    copy[params].product_cnt = e.target.value
+    copy[index].product_cnt = parseInt(e.target.value)
     selectChange(copy)
 
     if(findProduct.BlackFriday === 'O'){
-      product_sum += copy[params].product_cnt * (findProduct.price / 2)
+      product_sum += copy[index].product_cnt * (findProduct.price / 2)
       sumChange(product_sum)
     } else{
-      product_sum += copy[params].product_cnt * findProduct.price
+      product_sum += copy[index].product_cnt * findProduct.price
       sumChange(product_sum)
     }
+    console.log(select)
   }
 
-  function delete_select(params){
+  function delete_select(index){
     let copy = [...select]
     let product_count = cnt
     let product_sum = sum
     if(findProduct.BlackFriday === 'O'){
-      product_sum -= copy[params].product_cnt * (findProduct.price / 2)
+      product_sum -= copy[index].product_cnt * (findProduct.price / 2)
       sumChange(product_sum)
     } else{
-      product_sum -= copy[params].product_cnt * findProduct.price
+      product_sum -= copy[index].product_cnt * findProduct.price
       sumChange(product_sum)
     }
-    product_count -= copy[params].product_cnt
-    copy.splice(params, 1)
+    product_count -= copy[index].product_cnt
+    copy.splice(index, 1)
     selectChange(copy)
     cntChange(product_count)
   }
 
-  function push_data_to_cart(e){
-    let check = false
-    console.log(check)
-    let copy = props.cart
-    const select = {product_id: id, product_size: e.target.value}
-    for(let i=0;i<copy.length;i++){
-      if (copy[i].product_id === select.product_id & copy[i].product_size === select.product_size){
-        check = true
-        break
+  function push_data_to_cart(){
+    let copy = [...props.cart]
+    let check = []
+    for(let i=0;i<select.length;i++){
+      check.push(-1)
+    }
+    for(let i=0;i<select.length;i++){
+      for(let j=0;j<copy.length;j++){
+        if(select[i].product_id === copy[j].product_id && select[i].product_size === copy[j].product_size){
+          check[i] = j
+          break
+        }
       }
     }
-    if(!check){
-      copy.push(select)
-    }
+    check.map((duplicate, index) => {
+      if(duplicate !== -1){
+        copy[duplicate].product_cnt += select[index].product_cnt
+      } else{
+        copy.push(select[index])
+      }
+    })
+    console.log(check)
     console.log(copy)
+    props.cartChange(copy)
   }
 
   return(
@@ -144,7 +154,7 @@ function Detail(props){
                       {
                         findProduct.size.map((shoes_size) => {
                           return(
-                            <option value={shoes_size}>{shoes_size}</option>
+                            <option value={shoes_size} key="shoes_size">{shoes_size}</option>
                           )
                         })
                       }
@@ -189,7 +199,7 @@ function Detail(props){
             <p className='container-item-info-total'>total: {sum}({cnt})</p>
             <div className='container-item-info-btns'>
               <div className='container-item-info-btns-buy'>BUY NOW</div>
-              <div className='container-item-info-btns-cart'>ADD TO CART</div>
+              <div className='container-item-info-btns-cart' onClick={push_data_to_cart}>ADD TO CART</div>
             </div>
           </div>
         </div>
