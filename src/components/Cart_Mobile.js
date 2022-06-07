bf205e64ccabf7361c74ef994645fdddecf9d437
guswnsakvk <guswnsakvk1@naver.com>
2022-06-07 17:30:27 +0900

    cart 모바일 상품 보여주기 틀 잡기

--- src/components/Cart_Mobile.js
+++ src/components/Cart_Mobile.js
@@ -1,12 +1,15 @@
 import "../style/Cart_Mobile.css"
 
-function Cart_Mobile(){
+function Cart_Mobile(props){
   function cart_list_btn(){
-    const div = document.querySelector(".cart-mobile-header-btn")
-    if(div.innerText === "∨"){
-      div.innerText = "∧"
+    const header_btn = document.querySelector(".cart-mobile-header-btn")
+    const cart_product_list = document.querySelector(".cart-mobile-products-list")
+    if(header_btn.innerText === "∨"){
+      header_btn.innerText = "∧"
+      cart_product_list.style.display = "block"
     } else{
-      div.innerText = "∨"
+      header_btn.innerText = "∨"
+      cart_product_list.style.display = "none"
     }
   }
 
@@ -14,7 +17,29 @@ function Cart_Mobile(){
     <>
       <div className="cart-mobile-header">
         <div className="cart-mobile-title">장바구니 상품</div>
-        <div className="cart-mobile-header-btn" onClick={cart_list_btn}>∨</div>
+        <div className="cart-mobile-header-btn" onClick={cart_list_btn}>∧</div>
+      </div>
+      <div className="cart-mobile-products-list">
+        {
+          props.cartCopy.map((product, i) => {
+            return(
+              <div className="cart-mobile-products-list-product">
+                <div className="cart-mobile-products-list-product-header">
+                  <div className="cart-mobile-products-list-product-header-group">
+                    <input className="cart-mobile-products-list-product-header-checkbox" type={"checkbox"}></input>
+                    <span>[{product.product_size}] {product.product_name}</span>
+                  </div>
+                  <div className="cart-mobile-products-list-product-header-remove-btn">X</div>
+                </div>
+                <div className="cart-mobile-products-list-product-body">
+                  <div className="cart-mobile-products-list-product-body-img" style={{backgroundImage: `url(${require(`../image/product${parseInt(product.product_id)+1}.jpg`)})`}}></div>
+                  <div className="cart-mobile-products-list-product-body-info"></div>
+                </div>
+                <div className="cart-mobile-products-list-product-foot"></div>
+              </div>
+            )
+          })
+        }
       </div>
     </>
   )
