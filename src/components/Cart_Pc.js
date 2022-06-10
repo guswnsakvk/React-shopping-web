a4283235130e2e808ceacfe012946e29480b88ed
guswnsakvk <guswnsakvk1@naver.com>
2022-06-10 16:32:25 +0900

    장바구니가 비었을 경우 문구 띄우기

--- src/components/Cart_Pc.js
+++ src/components/Cart_Pc.js
@@ -9,58 +9,65 @@ function Cart_Pc(props){
   const week = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
 
   return(
-    <table className='cart-pc-container-table'>
-      <thead className='cart-pc-container-table-head'>
-        <tr>
-          <th className='cart-pc-container-table-head-product-info'>상품 정보</th>
-          <th className='cart-pc-container-table-head-product-cnt'>수량</th>
-          <th className='cart-pc-container-table-head-product-price'>상품 금액</th>
-          <th className='cart-pc-container-table-head-product-delivery'>배송 정보</th>
-          <th className='cart-pc-container-table-head-product-order'>주문</th>
-        </tr>
-      </thead>
-      <tbody className='cart-pc-container-table-body'>
-        {
-          props.cartCopy.map((product, i) => {
-            return(
-              <tr>
-                <td className='cart-pc-container-table-body-product'>
-                  <input onClick={() => {props.select_cart_product(i)}} className='cart-pc-container-table-body-product-checkBox' type={"checkbox"}></input>
-                  <div className='cart-pc-container-table-body-product-img' style={{backgroundImage: `url(${require(`../image/product${parseInt(product.product_id)+1}.jpg`)})`}}></div>
-                  <div className='cart-pc-container-table-body-product-info'>
-                    <p>[{product.product_size}] {product.product_name}</p>
-                    {
-                      product.black_friday === 'O'
-                      ?
-                        <p><span className='cart-pc-container-table-body-product-sale'>{product.product_price * 2}</span> → {product.product_price}</p>
-                      :
-                        <p>{product.product_price}</p>
-                    }
-                  </div>
-                </td>
-                <td>
-                  <input className='cart-pc-container-table-body-product-cnt' onChange={(e) => {props.set_cart_value(i, e)}} min={1} type={"number"} value={product.product_cnt}></input>
-                </td>
-                <td>{product.product_price}</td>
-                {
-                  product.product_quick === 'O'
-                  ? <td>
-                      <p>내일</p>
-                      <p className="cart-pc-container-table-body-date">({month}/{date}, {week[day]})</p>
-                      <p className="cart-pc-container-table-body-date">도착예정</p>
-                    </td>
-                  : <td>기본배송</td>
-                }
-                <td>
-                  <div className='cart-pc-container-table-body-product-order'>주문하기</div>
-                  <div className='cart-pc-container-table-product-remove' onClick={() => {props.remove_cart_item(i)}}>삭제하기</div>
-                </td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+    <>
+      <table className='cart-pc-container-table'>
+        <thead className='cart-pc-container-table-head'>
+          <tr>
+            <th className='cart-pc-container-table-head-product-info'>상품 정보</th>
+            <th className='cart-pc-container-table-head-product-cnt'>수량</th>
+            <th className='cart-pc-container-table-head-product-price'>상품 금액</th>
+            <th className='cart-pc-container-table-head-product-delivery'>배송 정보</th>
+            <th className='cart-pc-container-table-head-product-order'>주문</th>
+          </tr>
+        </thead>
+        <tbody className='cart-pc-container-table-body'>
+          {
+            props.cartCopy.map((product, i) => {
+              return(
+                <tr>
+                  <td className='cart-pc-container-table-body-product'>
+                    <input onClick={() => {props.select_cart_product(i)}} className='cart-pc-container-table-body-product-checkBox' type={"checkbox"}></input>
+                    <div className='cart-pc-container-table-body-product-img' style={{backgroundImage: `url(${require(`../image/product${parseInt(product.product_id)+1}.jpg`)})`}}></div>
+                    <div className='cart-pc-container-table-body-product-info'>
+                      <p>[{product.product_size}] {product.product_name}</p>
+                      {
+                        product.black_friday === 'O'
+                        ?
+                          <p><span className='cart-pc-container-table-body-product-sale'>{product.product_price * 2}</span> → {product.product_price}</p>
+                        :
+                          <p>{product.product_price}</p>
+                      }
+                    </div>
+                  </td>
+                  <td>
+                    <input className='cart-pc-container-table-body-product-cnt' onChange={(e) => {props.set_cart_value(i, e)}} min={1} type={"number"} value={product.product_cnt}></input>
+                  </td>
+                  <td>{product.product_price}</td>
+                  {
+                    product.product_quick === 'O'
+                    ? <td>
+                        <p>내일</p>
+                        <p className="cart-pc-container-table-body-date">({month}/{date}, {week[day]})</p>
+                        <p className="cart-pc-container-table-body-date">도착예정</p>
+                      </td>
+                    : <td>기본배송</td>
+                  }
+                  <td>
+                    <div className='cart-pc-container-table-body-product-order'>주문하기</div>
+                    <div className='cart-pc-container-table-product-remove' onClick={() => {props.remove_cart_item(i)}}>삭제하기</div>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+      {
+        props.cartCopy.length === 0
+        ? <div className="cart-pc-emtpy">장바구니가 비어 있습니다</div>
+        : null
+      }
+    </>
   )
 }
 
